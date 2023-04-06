7444e1565eecbc23c0496a5de0932b4893f93be7
nataha11 <79098507+nataha11@users.noreply.github.com>
2023-04-07 01:33:35 +0300

    delete comment

--- solution.py
+++ solution.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 1283808304 # Ваш chat ID, не меняйте название переменной
+chat_id = 1283808304
 
 def solution(x: np.array) -> float:
     n = len(x)
