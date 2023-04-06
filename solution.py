d1c45cd4ccdebde8c7d384fbefaa5961f173ac07
nataha11 <79098507+nataha11@users.noreply.github.com>
2023-04-06 23:26:59 +0300

    точечная оценка, метод моментов

--- solution.py
+++ solution.py
@@ -5,7 +5,9 @@ import numpy as np
 chat_id = 1283808304 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    n = len(x)
+    time = 10
+    errors = np.random.exponential(1, n) - 15
+    v_real = x + errors
+    a = (1/(n*time)) * np.sum(v_real)
+    return a
